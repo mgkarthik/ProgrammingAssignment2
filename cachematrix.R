427c2d9860ec0e038fb43844615a1870171e2c31
mgkarthik <mg.karthik1992@gmail.com>
2014-11-20 21:41:31 +0530

    Coded Functions
    The functions were coded to compute inverse of a matrix if it hasn't
    already been computed.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function sets and gets the values of a matrix and its inverse.
+## The second function checks if the inverse of the matrix from function 1 has been computed,
+## if it has, then the second function retrieves this inverse, else, it computes the inverse and returns it.
 
-## Write a short comment describing this function
+## This function sets and gets the values of a matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i<-NULL
+  set<-function(y)
+  {x<<-y
+  i<<-NULL}
+  get<-function() x
+  setinv<-function(inv) i<<-inv
+  getinv<-function() i
+  list(set=set,get=get,setinv=setinv,getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## This function checks if the inverse of the matrix has already been computed. If it has, then it retrieves it from the cache, else it computes the inverse and returns it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+i<-x$getinv()
+if(!is.null(i))
+{message("Getting cached data")
+return(i)}
+else
+{data<-x$get()
+i<-solve(data,...)
+x$setinv(i)
+i}}
